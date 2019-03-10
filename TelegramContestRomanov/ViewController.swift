a91e19ac85a057f40e50236a990eded77685de7f
Filipp <filipp.romano@gmail.com>
2019-03-10 13:35:40 +0300

    Initial Commit

---
+++ TelegramContestRomanov/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TelegramContestRomanov
+//
+//  Created by Филипп on 10.03.2019.
+//  Copyright © 2019 Филипп. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
